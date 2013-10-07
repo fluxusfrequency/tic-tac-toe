0be0ee7d91668cbad356bbf08d49d27af9ac309f
Ben Lewis <bennlewis@gmail.com>
2013-10-07 10:16:15 -0600

    built board

--- view/basic_view.rb
+++ view/basic_view.rb
@@ -3,13 +3,26 @@ require 'ruby-processing'
 class TicTacToe < Processing::App
 
   def setup
+    size 800, 800
     background(0, 0, 0)
-    ellipse_mode CENTER
-    rect_mode CENTER
     smooth
+    create_lines
   end
 
   def draw
+    stroke 256,256,256
+    create_lines
+  end
+
+  def create_lines
+    # line 150, 150, 150, 600
+    line 301, 133, 301, 666
+    line 488, 133, 488, 666
+    line 133, 301, 666, 301
+    line 133, 488, 666, 488
+
+    # rect 133,133,533,533
+
   end
 
 end
