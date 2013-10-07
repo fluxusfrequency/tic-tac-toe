4db1cc287a78eb5ef3b84f39d0bb8ec0c28cbf8e
Ben Lewis <bennlewis@gmail.com>
2013-10-07 10:57:34 -0600

    built squares 5-8 as methods returning nested arrays

--- view/basic_view.rb
+++ view/basic_view.rb
@@ -5,17 +5,14 @@ class TicTacToe < Processing::App
   def setup
     size 800, 800
     background(0, 0, 0)
-    smooth
-    create_lines
   end
 
   def draw
-    stroke 256,256,256
     create_lines
   end
 
   def create_lines
-    # line 150, 150, 150, 600
+    stroke 256,256,256
     line 301, 133, 301, 666
     line 488, 133, 488, 666
     line 133, 301, 666, 301
@@ -25,7 +22,22 @@ class TicTacToe < Processing::App
 
   end
 
+  def square_5
+    [[488, 301],[666, 301],[488, 488],[666, 488]]
+  end
+
+  def square_6
+    [[133, 488],[301, 488],[133, 666],[301, 666]]
+  end
+
+  def square_7
+    [[301, 488],[488, 488], [301, 666], [488, 666]]
+  end
+
+  def square_8
+    [[488, 488],[666, 488], [488, 666], [666, 666]]
+  end
+
 end
 
-TicTacToe.new(:width => 800, :height => 800,
-  :title => "TicTacToe", :full_screen => false)
+TicTacToe.new(:title => "TicTacToe", :full_screen => false)
